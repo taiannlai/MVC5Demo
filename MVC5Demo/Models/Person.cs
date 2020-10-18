373dbe008459300a2bcf466e67e99cac7779a9d9
taiannlai <30769612+taiannlai@users.noreply.github.com>
2020-10-18 15:54:38 +0800

    限制age的上下限

--- MVC5Demo/Models/Person.cs
+++ MVC5Demo/Models/Person.cs
@@ -10,9 +10,11 @@ namespace MVC5Demo.Models
     {
         public int Id { get; set; }
         [Required(ErrorMessage ="請輸入姓名")]
+        [MaxLength(5,ErrorMessage ="姓名長度最長{1}")]
+        [MinLength(2,ErrorMessage = "姓名長度最短{1}")]
         public string Name { get; set; }
         [Required(ErrorMessage = "請輸入年紀")]
-        [Range(18,99,ErrorMessage = "最少需在18歲以上，99歲以下")]
+        [Range(18,99,ErrorMessage = "最少需在{1}歲以上，{2}歲以下")]
         public int Age { get; set; }
     }
 }
