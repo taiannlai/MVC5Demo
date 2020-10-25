e46aa3051f948cf920d7c841da8885f27d2d1882
taiannlai <30769612+taiannlai@users.noreply.github.com>
2020-10-25 11:13:29 +0800

    手動進行建立Partial，專門放department的驗証屬性

---
+++ MVC5Demo/Models/Department.Partial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Demo.Models
+{
+    [MetadataType(typeof(DepartmentMetadata))]
+    public partial class Department //因為這是department的擴充，所以該行要跟department model一樣
+    {
+        public class DepartmentMetadata
+        {
+            public int DepartmentID { get; set; }
+            [Required]
+            public string Name { get; set; }
+            [Required]
+            public decimal Budget { get; set; }
+            [Required]
+            [DataType(DataType.Date)]
+            [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+            public Nullable<System.DateTime> StartDate { get; set; }
+            [Required]
+            public Nullable<int> InstructorID { get; set; }
+            public byte[] RowVersion { get; set; }
+        }
+    }
+}
--- MVC5Demo/Models/Department.cs
+++ MVC5Demo/Models/Department.cs
@@ -24,8 +24,6 @@ namespace MVC5Demo.Models
         public int DepartmentID { get; set; }
         public string Name { get; set; }
         public decimal Budget { get; set; }
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> StartDate { get; set; }
         public Nullable<int> InstructorID { get; set; }
         public byte[] RowVersion { get; set; }
